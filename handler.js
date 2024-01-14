4afe2d6294652a6b3e689e81e0b0ff9db49997fa
kaplantm <tonarie.k@gmail.com>
2024-01-14 13:13:39 -0500

    make date less prominent

--- handler.js
+++ handler.js
@@ -3,6 +3,5 @@ const { updateBrightsideData } = require("./lib/helper.js");
 
 module.exports.handleUpdateBrightsideData = async (event) => {
   const response = await updateBrightsideData()
-  console.log({ response })
   return response;
 };
