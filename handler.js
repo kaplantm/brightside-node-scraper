0d21ba6dbd379a0625332017d076766b628295e1
kaplantm <tonarie.k@gmail.com>
2024-01-13 22:29:31 -0500

    refactor to html only

--- handler.js
+++ handler.js
@@ -1,50 +1,8 @@
 "use strict";
-const request = require("axios");
-const S3 = require("./lib/aws-s3-client.js");
-const { getPositionFromHTML } = require("./helpers");
+const { updateBrightsideData } = require("./lib/helper.js");
 
-const s3Client = new S3.S3Client(
-  process.env.ACCESS_KEY_ID,
-  process.env.SECRET_ACCESS_KEY
-);
-
-module.exports.updateBrightsideData = async (event) => {
-  try {
-    return await request("https://www.officialcharts.com/charts/").then(
-      async ({ data }) => {
-        const result = getPositionFromHTML(data);
-        const stringifiedResult = JSON.stringify({
-          ...result,
-          updated_at: new Date().getTime(),
-        });
-        const s3PutRequest = s3Client.createPutPublicJsonRequest(
-          "ismrbrightsidestillintheukcharts.com/assets",
-          "brightside.json",
-          stringifiedResult
-        );
-        const s3Response = await s3Client.put(s3PutRequest);
-
-        console.log({ response: stringifiedResult, type: "success" });
-
-        return {
-          statusCode: 200,
-          headers: {},
-          body: stringifiedResult,
-          isBase64Encoded: false,
-        };
-      }
-    );
-  } catch (error) {
-    const body = {
-      message: "An error occurred.",
-      error,
-    };
-    console.log({ response: body, type: "error" });
-    return {
-      statusCode: 500,
-      headers: {},
-      body: JSON.stringify(body),
-      isBase64Encoded: false,
-    };
-  }
+module.exports.handleUpdateBrightsideData = async (event) => {
+  const response = await updateBrightsideData()
+  console.log({ response })
+  return response;
 };
--- readMe.md
+++ readMe.md
@@ -1,1 +1,10 @@
 Lambda function node webscraper that finds the position of Mr. Brightside in the [UK Official Charts](https://www.officialcharts.com/charts/) and publishes that data to an json file in an s3 bucket.
+
+
+Using the [Serverless Framework](https://www.npmjs.com/package/serverless)
+
+
+## Running locally
+
+`npm install`
+`sls invoke local -f handleUpdateBrightsideData`
